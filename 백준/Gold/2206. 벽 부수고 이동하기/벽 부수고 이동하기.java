01c67b51d46155aa78c3bd9bac2fd348285bbf50
HyunjungKim323 <119280993+HyunjungKim323@users.noreply.github.com>
2024-02-18 23:32:50 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 908 ms, Memory: 89520 KB -BaekjoonHub

---
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.java
@@ -0,0 +1,85 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+	
+	static int N,M;
+	static int[][] map;
+	static int[][] distance;
+	static int[][] distance2;
+	static int[] dx = {-1,1,0,0};
+	static int[] dy = {0,0,-1,1};
+	static Queue<int[]> q;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st;
+		st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		map = new int[N][M];
+		distance = new int[N][M];
+		distance2 = new int[N][M];
+		q = new ArrayDeque<>();
+		
+		for(int i=0;i<N;i++) {
+			String line = br.readLine();
+			for(int j=0;j<M;j++) {
+				map[i][j] = line.charAt(j)-'0';
+				distance[i][j] = Integer.MAX_VALUE;
+				distance2[i][j] = Integer.MAX_VALUE;
+			}
+		}
+		q.offer(new int[] {0,0,1,0});
+		distance[0][0] = 1;
+		distance2[0][0] = 1;
+		bfs();
+		int ans = Integer.MAX_VALUE;
+		ans = Math.min(ans, Math.min(distance[N-1][M-1], distance2[N-1][M-1]));
+		
+		if(ans == Integer.MAX_VALUE ) {
+			bw.write("-1"+"\n");
+		}else {
+			bw.write(ans+"\n");
+		}
+		bw.flush();
+		
+	}
+	
+	static void bfs() {
+		while(!q.isEmpty()) {
+			int[] t = q.poll();
+			int x =t[0];
+			int y =t[1];
+			int time =t[2];
+			int pass = t[3];
+			for(int i=0;i<4;i++) {
+				int nx = x+dx[i];
+				int ny = y+dy[i];
+				if(pass==0) {
+					if(nx>=0&&nx<N&&ny>=0&&ny<M&&time+1<distance[nx][ny]&&map[nx][ny]==0) {
+						distance[nx][ny]=time+1;
+						q.offer(new int[] {nx,ny,time+1,0});
+					}else if(nx>=0&&nx<N&&ny>=0&&ny<M&&map[nx][ny]==1&&time+1<distance2[nx][ny]){
+						distance2[nx][ny]= time+1;
+						q.offer(new int[] {nx,ny,time+1,1});
+					}
+				}else {
+					if(nx>=0&&nx<N&&ny>=0&&ny<M&&time+1<distance2[nx][ny]&&map[nx][ny]==0) {
+						distance2[nx][ny]=time+1;
+						q.offer(new int[] {nx,ny,time+1,1});
+					}
+				}
+				
+			}
+		
+		}
+	}
+	
+}
