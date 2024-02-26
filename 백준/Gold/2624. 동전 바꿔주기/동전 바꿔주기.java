9b6dcdd16c84caf75064bd6c20ef79c088eaa1c3
HyunjungKim323 <119280993+HyunjungKim323@users.noreply.github.com>
2024-02-26 14:46:13 +0900

    [Gold IV] Title: 동전 바꿔주기, Time: 236 ms, Memory: 19576 KB -BaekjoonHub

---
+++ 백준/Gold/2624. 동전 바꿔주기/동전 바꿔주기.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		int k = sc.nextInt();
+		int[][] dp = new int[k+1][t+1];
+		dp[0][0] = 1;
+		for(int i=1;i<=k;i++) {
+			int coinPrice = sc.nextInt();
+			int coinCount = sc.nextInt();
+			for(int value =0; value<=t; value++) {
+				for(int cnt =0;cnt<=coinCount;cnt++) {
+					
+					int nextValue = value+coinPrice *cnt;
+					
+					if(nextValue<=t) 
+						dp[i][nextValue]+= dp[i-1][value];
+					else {
+						break;
+					}
+				}
+			}
+			
+		}
+		
+		System.out.println(dp[k][t]);
+		
+		
+	}
+}
+	
